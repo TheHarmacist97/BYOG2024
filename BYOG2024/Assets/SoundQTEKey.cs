5078b14aca985bc31896522c3cf17656a73d68a3
Varun Vyas <varunvyas55@gmail.com>
2024-09-20 22:59:17 +0530

    Completed sound design QTE

--- BYOG2024/Assets/SoundQTEKey.cs
+++ BYOG2024/Assets/SoundQTEKey.cs
@@ -1,28 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using QTEs.SoundDesignQTE;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public enum QTEKey
 {
-    Up,
-    Down,
-    Left,
-    Right
+    W,
+    S,
+    A,
+    D
 }
 
 public class SoundQTEKey : MonoBehaviour
 {
-    public QTEKey _qteKey;
-    public Collider2D _keyCollider;
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private float _speed;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private QTEKey _targetKey;
+    public QTEKey TargetKey => _targetKey;
 
-    public void SetUp(Color color, float speed)
+    public void SetUp(Color color, float speed, QTEKey keyQueueElement)
     {
         _spriteRenderer.color = color;
         _speed = speed;
+        _targetKey = keyQueueElement;
+        _text.color = color;
+        _text.text = keyQueueElement.ToString();
+    }
+
+    public void KillSelf(bool userSucceeded)
+    {
+        //do some polish here
+        gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 
     public void Update()
